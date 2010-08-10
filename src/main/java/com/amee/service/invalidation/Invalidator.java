85f23c83606e715f2a9689f3059f49fac06e0f39
Diggory Briercliffe <dig@amee.cc>
2010-08-10 17:28:15 +0100

    INFRA-326 - Trigger DC index rebuild (pre data load)

--- src/main/java/com/amee/service/invalidation/Invalidator.java
+++ src/main/java/com/amee/service/invalidation/Invalidator.java
@@ -2,11 +2,8 @@ package com.amee.service.invalidation;
 
 import com.amee.domain.AMEEEntityReference;
 import com.amee.domain.ObjectType;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.ConnectionParameters;
-import com.rabbitmq.client.MessageProperties;
+import com.rabbitmq.client.*;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -15,6 +12,7 @@ public class Invalidator {
     public final static Invalidator INSTANCE = new Invalidator();
 
     public static void main(String[] args) {
+        System.out.println("Starting...");
         try {
             if (args.length == 7) {
                 INSTANCE.sendMessage(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
@@ -22,8 +20,13 @@ public class Invalidator {
                 System.out.println("Not enough parameters.");
             }
         } catch (IOException e) {
-            System.out.println("Caught IOException: " + e);
+            if (e.getCause() == null) {
+                System.out.println("Caught IOException: " + e.getMessage());
+            } else {
+                System.out.println("Caught IOException: " + e.getMessage() + " Cause: " + e.getCause().getMessage());
+            }
         }
+        System.out.println("...done.");
     }
 
     public void sendMessage(
@@ -34,27 +37,31 @@ public class Invalidator {
             String password,
             String entityType,
             String entityUid) throws IOException {
-        // Create the InvalidationMessage.
+        System.out.println("    1) Create the InvalidationMessage.");
         InvalidationMessage message =
                 new InvalidationMessage(
                         this,
                         new AMEEEntityReference(ObjectType.valueOf(entityType), entityUid));
-        // Configure RabbitMQ.
+        System.out.println("    2) Configure RabbitMQ.");
         ConnectionParameters connectionParameters = new ConnectionParameters();
         connectionParameters.setUsername(username);
         connectionParameters.setPassword(password);
         ConnectionFactory connectionFactory = new ConnectionFactory(connectionParameters);
         Connection connection = connectionFactory.newConnection(hostName, Integer.valueOf(portNumber));
         Channel channel = connection.createChannel();
-        // Send the message.
+        System.out.println("    3) Send the message.");
+        String messageText = message.getMessage();
+        System.out.println("        Message: " + messageText);
+        System.out.println("        Destination: platform." + scope + ".invalidation");
         channel.basicPublish(
                 "platform." + scope + ".invalidation",
                 "platform." + scope + ".invalidation",
                 false,
                 false,
                 MessageProperties.PERSISTENT_TEXT_PLAIN,
-                message.getMessage().getBytes());
-        // We're done with the channel.
+                messageText.getBytes());
+        System.out.println("    4) Close the channel & connection.");
         channel.close();
+        connection.close();
     }
 }
