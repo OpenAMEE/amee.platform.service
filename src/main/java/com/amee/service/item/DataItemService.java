369761c749ba97b42e455abb4c89e1193590a5a5
Paul Carey <paul.p.carey@gmail.com>
2010-10-19 16:29:22 +0100

    Added speculative implementation of DataItemService#getLabel.

--- src/main/java/com/amee/service/item/DataItemService.java
+++ src/main/java/com/amee/service/item/DataItemService.java
@@ -1,7 +1,12 @@
 package com.amee.service.item;
 
 import com.amee.domain.IDataItemService;
+import com.amee.domain.data.ItemDefinition;
+import com.amee.domain.data.ItemValue;
+import com.amee.domain.item.BaseItemValue;
+import com.amee.domain.item.data.BaseDataItemTextValue;
 import com.amee.domain.item.data.NuDataItem;
+import com.amee.domain.sheet.Choice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +15,9 @@ public class DataItemService extends ItemService implements IDataItemService {
 
     @Autowired
     private DataItemServiceDAO dao;
+
+    @Autowired
+    private ItemService itemService;
 
     public NuDataItem getItemByUid(String uid) {
         NuDataItem dataItem = dao.getItemByUid(uid);
@@ -21,7 +29,29 @@ public class DataItemService extends ItemService implements IDataItemService {
     }
 
     public String getLabel(NuDataItem dataItem) {
-        // TODO: See com.amee.domain.data.LegacyDataItem#getLabel.
+        String label = "";
+        BaseDataItemTextValue itemValue = null;
+
+        ItemDefinition itemDefinition = dataItem.getItemDefinition();
+        for (Choice choice : itemDefinition.getDrillDownChoices()) {
+            itemValue = (BaseDataItemTextValue) itemService.getItemValue(dataItem, choice.getName());
+            if ((itemValue != null) &&
+                    (itemValue.getValue().length() > 0) &&
+                    !itemValue.getValue().equals("-")) {
+                if (label.length() > 0) {
+                    label = label.concat(", ");
+                }
+                label = label.concat(itemValue.getValue());
+            }
+        }
+        if (label.length() == 0) {
+            label = dataItem.getDisplayPath();
+        }
+        return label;
+    }
+
+    private ItemValue getItemValue(String choiceName) {
         throw new UnsupportedOperationException();
     }
+
 }
