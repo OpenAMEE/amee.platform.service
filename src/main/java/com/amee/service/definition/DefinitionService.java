15e7b1b35b334b3fe62cd0e90af5ddaf1f98c18c
David Keen <david.keen@amee.com>
2011-02-03 11:12:13 +0000

    Add some javadoc.

--- src/main/java/com/amee/service/definition/DefinitionService.java
+++ src/main/java/com/amee/service/definition/DefinitionService.java
@@ -232,6 +232,11 @@ public class DefinitionService extends BaseService implements ApplicationListene
         dao.save(returnValueDefinition);
     }
 
+    /**
+     * Set all sibling ReturnValueDefinition defaultType values to false.
+     *
+     * @param returnValueDefinition the ReturnValueDefinition that is the new default type.
+     */
     public void unsetDefaultTypes(ReturnValueDefinition returnValueDefinition) {
         if (returnValueDefinition.isDefaultType()) {
             for (ReturnValueDefinition rvd : returnValueDefinition.getItemDefinition().getReturnValueDefinitions()) {
