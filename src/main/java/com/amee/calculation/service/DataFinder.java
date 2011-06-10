6d01c864079ad4dbfb2a62aadff1d2e26e545676
David Keen <david.keen@amee.com>
2011-06-10 16:43:59 +0100

    Add com.amee.calculation.service.DataFinder#getDataSeries method. PL-10852.

--- src/main/java/com/amee/calculation/service/DataFinder.java
+++ src/main/java/com/amee/calculation/service/DataFinder.java
@@ -25,6 +25,10 @@ import com.amee.domain.item.BaseItemValue;
 import com.amee.domain.item.data.DataItem;
 import com.amee.domain.sheet.Choice;
 import com.amee.domain.sheet.Choices;
+import com.amee.platform.science.Amount;
+import com.amee.platform.science.DataPoint;
+import com.amee.platform.science.DataSeries;
+import com.amee.platform.science.ExternalHistoryValue;
 import com.amee.service.data.DataService;
 import com.amee.service.data.DrillDownService;
 import org.apache.commons.logging.Log;
@@ -34,6 +38,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 @Scope("prototype")
@@ -57,6 +62,39 @@ public class DataFinder {
         super();
     }
 
+    /**
+     * Gets a DataSeries for a DataItemValue
+     * 
+     * @param path the DataCategory path, eg "home/energy/electricity"
+     * @param drillDown comma separated drilldown selection, eg "country=United Kingdom"
+     * @param name the item value path, eg "kgCO2PerKWh"
+     * @return a DataSeries for the DataItemValue.
+     */
+    public DataSeries getDataSeries(String path, String drillDown, String name) {
+        DataSeries series = new DataSeries();
+        DataItem dataItem = getDataItem(path, drillDown);
+        if (dataItem != null) {
+            // Get all the values
+            List<BaseItemValue> itemValues = dataItemService.getAllItemValues(dataItem, name);
+            for (BaseItemValue value : itemValues) {
+                if (ExternalHistoryValue.class.isAssignableFrom(value.getClass())) {
+                    series.addDataPoint(new DataPoint(((ExternalHistoryValue) value).getStartDate().toDateTime(), new Amount(value.getValueAsString())));
+                } else {
+                    series.addDataPoint(new DataPoint(new Amount(value.getValueAsString())));
+                }
+            }
+        }
+        log.debug("getDataSeries() - path: " + path + ", drillDown: " + drillDown + ", name: " + name + ", values: " + series);
+        return series;
+    }
+
+    /**
+     *
+     * @param path the DataCategory path, eg "home/energy/electricity"
+     * @param drillDown comma separated drilldown selection, eg "country=United Kingdom"
+     * @param name the item value path, eg "kgCO2PerKWh"
+     * @return the current value for the DataItemValue.
+     */
     public String getDataItemValue(String path, String drillDown, String name) {
         String value = null;
         BaseItemValue itemValue;
